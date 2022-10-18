e167b3ad8c419eff0a8c74aeba1544a757699432
sodaop <saiphirun_p@cmu.ac.th>
2022-10-18 15:31:20 +0700

    fix cors

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://52.0.124.81:8999',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
